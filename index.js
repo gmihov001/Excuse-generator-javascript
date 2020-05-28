f794f787c2e61af0cb42c0b4109b62e125674bac
George Mihov <gmihov001@gmail.com>
2020-05-28 03:14:13 +0000

    Updates

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const onLoad = () => {
     let what = ['eat','pissed','crushed','broked'];
     let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
     
-    some_excuse = who[Math.floor()];
+    some_excuse = who[Math.floor(Math.random(0, who.length-1)*10)] + what[Math.floor(Math.random(0, who.length-1)*10)] + when[Math.floor(Math.random(0, who.length-1)*10)];
 };
 
 let excuse = document.getElementById('excuse');
+document.getElementById("excuse").innerHTML = Math.random();
 
 excuse = onLoad;
 console.log(excuse.innerHTML);
