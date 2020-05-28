e3a15ded94ad74b7294be4b88c53c38006ac4a41
George Mihov <gmihov001@gmail.com>
2020-05-28 02:44:10 +0000

    Updates

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const onLoad = () => {
+    
+    let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['eat','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+    
+    some_excuse = who[Math.floor()];
+};
+
+let excuse = document.getElementById('excuse');
+
+excuse = onLoad;
+console.log(excuse.innerHTML);
